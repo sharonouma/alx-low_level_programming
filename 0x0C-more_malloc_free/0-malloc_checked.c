4a3cc6c5991503fa78514fb7aff23676c40821b3
Your Name <you@example.com>
2023-04-13 03:15:14 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,17 @@
 #include <stdlib.h>
-#include <stdio.h>
 /**
- *malloc_checked - allocates mem using malloc
- *
- *@b:int param
- *Return:pointer to allocated mem
- *
- */
+  * malloc_checked - allocates memory using malloc.
+  * @b: size of memory to be allocated.
+  *
+  * Return: pointer to allocated memory.
+  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *p = malloc(b);
 
-	p = malloc(b);
 	if (p == NULL)
-	{
 		exit(98);
-	}
+
 	return (p);
 }
 
