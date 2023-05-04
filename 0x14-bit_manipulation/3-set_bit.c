b58a84c2f1b436bfaac5ed7b0042cc95cb920a3f
Your Name <you@example.com>
2023-05-04 11:44:25 -0700

    commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	*n = (*n | m);
+
+	return (1);
+}
